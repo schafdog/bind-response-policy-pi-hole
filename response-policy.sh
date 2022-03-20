1f623f46cf5b8c5772d1ffffb8a5684df1b11874
Dennis schafroth <dennis@schafroth.com>
2022-03-20 11:54:57 +0100

    Reload on success only

--- response-policy.sh
+++ response-policy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ~/bin/response-policy.py > /var/named/rpz-adblock
-named-compilezone -f text -F raw -o /var/named/rpz-adblock.db "rpz-adblock" /var/named/rpz-adblock
-rndc reload rpz-adblock
+named-compilezone -f text -F raw -o /var/named/rpz-adblock.db "rpz-adblock" /var/named/rpz-adblock && rndc reload rpz-adblock
