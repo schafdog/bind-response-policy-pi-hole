c59a9fe6a526ae3d97828105eaea957abdfb9630
Dennis schafroth <dennis@schafroth.com>
2024-02-19 09:35:19 +0100

    Only run on active

--- response-policy.sh
+++ response-policy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-~/bin/response-policy.py > /var/named/rpz-adblock
-named-compilezone -f text -F raw -o /var/named/rpz-adblock.db "rpz-adblock" /var/named/rpz-adblock && rndc reload rpz-adblock
+FILE=/var/named/rpz-adblock
+FILTERED=${FILE}.filtered
+if iamactive.sh ; then
+    ~/bin/response-policy.py > $FILE
+    grep -v bbc /var/named/rpz-adblock |grep -v fundingchoicesmessages.google.com |grep -v redirectingat.com |grep -v googletagmanager > $FILTERED
+    /usr/sbin/named-compilezone -f text -F raw -o /var/named/rpz-adblock.db "rpz-adblock" $FILTERED && rndc reload rpz-adblock
+fi
